0613a68b7f3e05a72d7f99982bd3fab53f558b67
LFWLJ <3031646468@qq.com>
2026-09-23 14:35:59 +0800

    feat/groupByDate(...)

--- gov-common/src/main/java/com/gov/common/log/OperLogMapper.java
+++ gov-common/src/main/java/com/gov/common/log/OperLogMapper.java
@@ -2,9 +2,22 @@ package com.gov.common.log;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.mybatis.spring.annotation.MapperScan;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 
 @Mapper
 @MapperScan({"com.gov.application.mapper", "com.gov.common.log"})
 public interface OperLogMapper extends BaseMapper<OperLog> {
+
+    @Select("SELECT DATE(create_time) AS day, COUNT(*) AS cnt FROM sys_oper_log " +
+            "WHERE tenant_id = #{tenantId} AND create_time >= #{start} AND create_time < #{end} " +
+            "GROUP BY DATE(create_time)")
+    List<Map<String, Object>> groupByDate(@Param("tenantId") String tenantId,
+                                          @Param("start") LocalDateTime start,
+                                          @Param("end") LocalDateTime end);
 }
